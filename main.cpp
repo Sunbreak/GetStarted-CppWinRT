277fa7e851fff6c280fd15fb397f9a850bbe77b4
wangkun <wangkun@woodemi.com>
2020-01-30 05:59:48 +0800

    GetStarted with `SyndicationClient`
    Signed-off-by: wangkun <wangkun@woodemi.com>

--- main.cpp
+++ main.cpp
@@ -2,10 +2,27 @@
 
 using namespace winrt;
 using namespace Windows::Foundation;
+using namespace Windows::Foundation::Collections;
+using namespace Windows::Web::Syndication;
 
 int main()
 {
-    init_apartment();
-    Uri uri(L"http://aka.ms/cppwinrt");
-    printf("Hello, %ls!\n", uri.AbsoluteUri().c_str());
+    winrt::init_apartment();
+
+    Uri rssFeedUri{ L"https://blogs.windows.com/feed" };
+    SyndicationClient syndicationClient;
+    SyndicationFeed syndicationFeed = syndicationClient.RetrieveFeedAsync(rssFeedUri).get();
+    // FIXME https://developercommunity.visualstudio.com/content/problem/886344/converter-lookup-fails-due-to-memory-access-violat.html
+    //for (const SyndicationItem syndicationItem : syndicationFeed.Items())
+    //{
+    //    winrt::hstring titleAsHstring = syndicationItem.Title().Text();
+    //    std::wcout << titleAsHstring.c_str() << std::endl;
+    //}
+    IVector<SyndicationItem> syndicationItemVector = syndicationFeed.Items();
+    for (uint32_t i = 0; i < syndicationItemVector.Size(); i++)
+    {
+        SyndicationItem syndicationItem = syndicationItemVector.GetAt(i);
+        hstring titleAsHstring = syndicationItem.Title().Text();
+        std::wcout << titleAsHstring.c_str() << std::endl;
+    }
 }
